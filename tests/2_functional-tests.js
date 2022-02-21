ca9592cae95d5aef611ea43e592ec88fcb504e5d
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-02-21 09:19:03 +0800

    test resolved

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -103,7 +103,7 @@ suite('Functional Tests', function() {
                 .put('/api/threads/test')
                 .send({
                     board: 'Functional Test Thread',
-                    thread_id: testMessageId
+                    report_id: testMessageId
                 })
                 .end(function(error, response) {
                     assert.equal(response.status, 200);
